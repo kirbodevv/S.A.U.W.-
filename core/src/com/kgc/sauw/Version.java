f968526465a3b7fc69d72fa32290f9b476e6fe9e
Kirill <kirbo06@mail.ru>
2021-09-12 11:37:51 +0200

    0.2.7.h1 Beta
    - Исправлен баг из-за которого начал вылетать интерфейс крафтинга
    - Исправлен вылет игры при создании нового мира
    - Удалены лишние символы

--- core/src/com/kgc/sauw/Version.java
+++ core/src/com/kgc/sauw/Version.java
@@ -1,6 +1,6 @@
 package com.kgc.sauw;
 
 public class Version {
-    public static String VERSION = "0.2.7 Beta";
+    public static String VERSION = "0.2.7.h1 Beta";
     public static int MOD_FORMAT = 2;
 }

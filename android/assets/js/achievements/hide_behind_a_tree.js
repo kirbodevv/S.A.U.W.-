859738c2ff0250d6d143dc51ade97264fd15e473
Kirill <kirbo06@mail.ru>
2021-12-14 14:51:34 +0100

    - fixed hide_behind_a_tree.js

--- android/assets/js/achievements/hide_behind_a_tree.js
+++ android/assets/js/achievements/hide_behind_a_tree.js
@@ -1,6 +1,6 @@
 function check(){
     var x = Player.getCurrentTileX();
-    var z = Player.getCurrentTileY();
+    var z = Player.getCurrentTileZ();
     if(Environment.getWorld().map.getTile(x, 0, z).id == ID.get("sauw", "block:tree")) return true;
     return false;
 }

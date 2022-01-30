2e8a618251aa0643019439806dbcf0cc16ed64c7
Kirill <kirbo06@mail.ru>
2022-01-30 13:10:11 +0100

    - commands refactoring

--- assets/js/commands.js
+++ assets/js/commands.js
@@ -1,27 +1,35 @@
-function give(id, count){
-	give("sauw", id, count);
+function give(id, count) {
+    give("sauw", id, count);
 }
-function give(package, id, count){
-	Player.inventory.addItem(package, id, count);
+
+function give(package_, id, count) {
+    Player.inventory.addItem(package_, "item:" + id, count);
 }
-function color(r, g, b){
-	GI.setConsoleTextColor(r, g, b);
+
+function color(r, g, b) {
+    GI.setConsoleTextColor(r, g, b);
 }
-function print(txt){
-	GI.consolePrint(txt);
+
+function print(txt) {
+    GI.consolePrint(txt);
 }
-function saveWorld(worldName){
-	World.save(worldName);
+
+function saveWorld(worldName) {
+    World.save(worldName);
 }
-function screenshot(screenshotName){
-	World.screenshot(screenshotName);
+
+function screenshot(screenshotName) {
+    World.screenshot(screenshotName);
 }
-function setTime(time){
+
+function setTime(time) {
     World.WorldTime.setTime(time);
 }
-function clear(){
+
+function clear() {
     Player.inventory.deleteItems();
 }
-function exit(){
+
+function exit() {
     com.badlogic.gdx.Gdx.app.exit();
 }

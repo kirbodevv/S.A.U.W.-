2c41cffe8f64ffd3b4a2d54628f278d49d8f1ef9
Kirill <kirbo06@mail.ru>
2022-02-17 20:52:06 +0100

    - updated version

--- core/src/com/kgc/sauw/Version.java
+++ core/src/com/kgc/sauw/Version.java
@@ -2,8 +2,8 @@ package com.kgc.sauw;
 
 //do not delete, this file was automatically generated
 public class Version {
-	public static String VERSION = "0.3-beta";
-	public static int CODE_VERSION = 0;
+	public static String VERSION = "0.3.h1-beta";
+	public static int CODE_VERSION = 1;
 	public static int MOD_FORMAT = 0;
 	public static int MODULE_FORMAT = 0;
 }

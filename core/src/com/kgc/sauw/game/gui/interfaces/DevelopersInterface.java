cac43784128219c9fb09d898ecacc9f92622e0e8
Kirill <kirbo06@mail.ru>
2022-06-20 16:45:49 +0200

    - added contributors parser

--- core/src/com/kgc/sauw/game/gui/interfaces/DevelopersInterface.java
+++ core/src/com/kgc/sauw/game/gui/interfaces/DevelopersInterface.java
@@ -1,6 +1,7 @@
 package com.kgc.sauw.game.gui.interfaces;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.jvmfrog.curve.registry.RegistryMetadata;
 import com.kgc.sauw.core.gui.Interface;
 import com.kgc.sauw.core.gui.InterfaceUtils;
@@ -21,7 +22,24 @@ public class DevelopersInterface extends Interface {
         Button kirboGamesLink = (Button) getElement("button.kirbogames_link");
         MultilineTextView devs = (MultilineTextView) getElement("text.devs");
 
-        devs.setText("[LINK]@KirboGames : \n\t\t- Programming, music, textures\n[LINK]@IbremMiner837 :\n\t\t- Textures");
+        String[] contributorsList = Gdx.files.internal("contributors/contributors.list")
+                .readString().split("\\r?\\n");
+
+        Contributor[] contributors = new Contributor[contributorsList.length];
+
+        for (int i = 0; i < contributors.length; i++) {
+            contributors[i] = new Contributor(contributorsList[i]);
+        }
+        contributors = sort(contributors);
+
+        StringBuilder devsText = new StringBuilder();
+
+        for (Contributor c : contributors) {
+            devsText.append("[LINK]@").append(c.name)
+                    .append("[BLACK], contributions: ").append(c.contributions).append("\n");
+        }
+
+        devs.setText(devsText.toString());
 
         jvmfrogLink.setDefaultText();
         kirboGamesLink.setDefaultText();
@@ -32,5 +50,41 @@ public class DevelopersInterface extends Interface {
         kirboGamesLink.addEventListener(() -> {
             Application.openURI("https://github.com/KirboGames");
         });
+    }
+
+    private static Contributor[] sort(Contributor[] contributors) {
+        for (int i = 0; i < contributors.length; i++) {
+            int max = contributors[i].contributions;
+            int max_i = i;
+
+            for (int j = i + 1; j < contributors.length; j++) {
+                if (contributors[j].contributions > max) {
+                    max = contributors[j].contributions;
+                    max_i = j;
+                }
+            }
+
+            if (i != max_i) {
+                Contributor tmp = contributors[i];
+                contributors[i] = contributors[max_i];
+                contributors[max_i] = tmp;
+            }
+        }
+        return contributors;
+    }
+
+    private static class Contributor {
+        public Texture avatar;
+        public String name;
+        public int contributions;
+
+        public Contributor(String data) {
+            String[] keys = data.split(":");
+
+            name = keys[0];
+            contributions = Integer.parseInt(keys[1]);
+            avatar = Resource.getTexture("contributors/" + name + ".jpg");
+
+        }
     }
 }
